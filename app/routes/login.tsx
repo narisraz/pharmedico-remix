f551deae4fd4dc6f4414a1030bcb9f4cbee12dda
narisraz <naris.razafimahatratra@gmail.com>
2022-03-25 18:04:24 +0300

    Add remix validated form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,37 +3,17 @@ import {ActionFunction, LoaderFunction} from "remix";
 import {authenticator} from "~/services/auth.server";
 import LabeledTextField from "~/core/components/LabeledTextField";
 import { Formik } from 'formik';
+import {Button} from "@mui/material";
 
 export default function Login() {
   const action = useActionData()
   console.log(JSON.stringify(action))
   return (
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-        <Form method="post">
-          <LabeledTextField name="email" label="Email" placeholder="Email" />
-          <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-          <button>Se connecter</button>
-        </Form>
-      )}
-    </Formik>
+    <Form method="post" action={"/login"}>
+      <LabeledTextField name="email" label="Email" placeholder="Email" />
+      <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
+      <Button type={"submit"}>Se connecter</Button>
+    </Form>
   );
 }
 
